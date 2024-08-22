97643d087317d3781c5b3102d6bb30a4e92b1d27
skoanton <antonhakanssonn@gmail.com>
2024-08-22 20:14:42 +0200

    lagt till drawer för mobilview när klickar på meny

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,16 @@
 type HeaderProps = {};
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
-
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 export default function Header({}: HeaderProps) {
   return (
     <header className="flex justify-between">
@@ -17,9 +26,36 @@ export default function Header({}: HeaderProps) {
             </Button>
           </li>
           <li>
-            <Button className="rounded-xl outline-4" variant="outline">
-              <Menu />
-            </Button>
+            <Drawer>
+              <DrawerTrigger className="rounded-xl outline-4">
+                <Menu />
+              </DrawerTrigger>
+              <DrawerContent>
+                <DrawerHeader className="grid grid-cols-3">
+                  <DrawerTitle className="col-start-2 self-center ">
+                    Menu
+                  </DrawerTitle>
+                  <DrawerClose className="justify-self-end">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-10 h-10"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"
+                      />
+                    </svg>
+                  </DrawerClose>
+                </DrawerHeader>
+                <ul className="flex flex-col gap-10 font-bold text-2xl items-center p-5 ">
+                  <li>Home</li>
+                  <li>About me</li>
+                  <li>Projects</li>
+                  <li>Contact me</li>
+                </ul>
+              </DrawerContent>
+            </Drawer>
           </li>
         </ul>
       </nav>
